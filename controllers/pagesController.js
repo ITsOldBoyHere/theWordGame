952c3e5487e36e4d7fb35ce03bfadaa69f3ef504
ITsOldBoyHere <rodzyneek@gmail.com>
2022-06-29 10:35:00 +0200

    duzo zmian

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -1,3 +1,4 @@
+const session = require("express-session")
 
 exports.index = (req, res) => {
     res.render('index', {
@@ -6,7 +7,11 @@ exports.index = (req, res) => {
         formPoints: req.flash('points'),
         formButton: req.flash('button')
     })
-    
+    console.log(req.session)
+    const session=req.session;
+
+        console.log(session.user);
+
    // res.send({ formButton: 'GeeksforGeeks' })
    
 }
