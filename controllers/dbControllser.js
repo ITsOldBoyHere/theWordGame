fd6843a205f05a2ec30eaabce0d41f8879970d22
ITsOldBoyHere <rodzyneek@gmail.com>
2022-07-14 22:50:13 +0200

    wyglad

--- controllers/dbControllser.js
+++ controllers/dbControllser.js
@@ -8,7 +8,6 @@ class personControll {
         try {
             const id = await personServ.createPerson(req.body)
             req.session.nazwa_uzykownika.push([[req.body.username.toUpperCase().trim()], id.id, req.body.level]);
-            //console.log(req.session.nazwa_uzykownika[0][2]) //level
             req.flash('nazwa_uzytkownika', req.session.nazwa_uzykownika[0][0])
             res.redirect('/nowagra')
         } catch (err) {
